6a3ed5b96a40d0b6aa1de4c75b79fb86ccbabd66
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-12 13:14:53 -0500

    Solved!

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
+    print ('Introduce a number')
+    number1=int(input())
+    print ('Introduce another number')
+    number2=int(input())
+    suma=number1+number2
+    resta=number1-number2
+    multiplication=number1*number2
+    
+    print (suma)
+    print (resta)
+    print (multiplication)
+    
+    
     pass
 
 
