885ef50dfd1e2825c01f999b8a49c19e7ff6456a
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-15 20:51:13 -0500

    Solved!

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,17 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
-    numero1 = int(input('Dame un número:'))
-    numero2 = int(input('Dame un número:'))
-    suma = numero1 + numero2
-    resta = numero1 - numero2
-    multiplicacion = numero1 * numero2
+    numero1= int(input("Dame un número: "))
+    numero2= int(input('Dame un número: "))
+    suma= numero1 + numero2
+    resta= numero1 - numero2
+    multiplicacion= numero1 * numero2
     
-    print('Suma:', suma)
-    print('Resta:', resta)
-    print('Multiplicación:', multiplicacion)
+    print("Suma:", suma)
+    print("Resta:", resta)
+    print("Multiplicación:", multiplicacion)
     
     
-    pass
 
 
 if __name__ == '__main__':
