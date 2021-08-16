0d90e7a32f978537c50c5ffa59eccb21b7cdd768
JFranciscoGR03 <88851030+JFranciscoGR03@users.noreply.github.com>
2021-08-15 21:26:42 -0500

    Solved!

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    pesoInicial= float(input("Dame el peso inicial: "))
+    pesoFinal= float(input("Dame el peso final: "))
+    meses= int(input("Dame la cantidad de meses: "))
+    
+    #Cálculo de peso
+    bajar= (pesoInicial - pesoFinal) / meses
+    print("Lo que debes bajar por mes es:", bajar)
     
 
 
